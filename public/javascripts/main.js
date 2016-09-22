588121bc22140216588e4600e2942f08beb7484e
Vivian Chen <vivch3n@gmail.com>
2016-09-22 13:16:03 -0700

    clean up js code a bit, add some margin to form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,9 @@
 var form = $('.signup-form'),
-    // button = $('.future-customer'),
     learnmore = $(' .js-future-customer, .js-learn-more');
 
 learnmore.on('click', function() {
-  // debugger;
   if ($(this).hasClass('js-learn-more')) {
     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-    console.log('matches js-learn-more!!!!');
   }
 
   form.show(600);
