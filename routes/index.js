cb904f23985c91261d821ec12e8eda524a2a9f6f
Nick Doering <tortilaman@gmail.com>
2016-09-26 19:58:01 -0700

    Update to try and make sure success message goes away on subsequent refreshes.

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,8 @@ router.get('/', function(req,res,next) {
   });
 
   req.session.errors = null;
+  if(req.session.success === true) req.session.success = false;
+  else req.session.success = null;
   req.session.success = null;
   req.session.errorName = null;
   req.session.errorEmail = null;
