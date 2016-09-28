7bc0ee4f648e248f22d74d56197f3a0b2cc924ca
Vivian Chen <vivch3n@gmail.com>
2016-09-28 13:13:47 -0700

    hide success notice after 5 seconds, hide form if form is successfully sent

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,30 @@
 // Handle scroll and show form
+$( document ).ready(function() {
+  var form = $('.signup-form'),
+      learnmore = $(' .js-future-customer, .js-learn-more');
 
-var form = $('.signup-form'),
-    learnmore = $(' .js-future-customer, .js-learn-more');
+  if ( $( ".notice-success" ).length ) {
+    form.hide();
+    scrollDown();
+    setTimeout(function () {
+        $('.notice-success').fadeOut();
+    }, 5000);
+  }
 
-learnmore.on('click', function() {
-  $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-  $(".arrow-down-container").addClass('bounce');
-  form.show(600);
+  var form = $('.signup-form'),
+      learnmore = $(' .js-future-customer, .js-learn-more');
 
-  setTimeout(function () {
-      $('.arrow-down-container').removeClass('bounce');
-  }, 2000);
+  learnmore.on('click', function() {
+    form.show(600);
+    scrollDown();
+    $(".arrow-down-container").addClass('bounce');
+    setTimeout(function () {
+        $('.arrow-down-container').removeClass('bounce');
+    }, 2000);
 
+  });
 });
 
-// $('.notice-success').delay(3000).hide();
+function scrollDown() {
+  $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+}
